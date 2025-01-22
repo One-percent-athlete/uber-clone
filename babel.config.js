e4cbc360521d3f8f09ba0ad8565935e17c597961
One-percent-athlete <one.percent.athlete@gmail.com>
2025-01-22 23:44:59 +0900

    Update babel confgi file

--- babel.config.js
+++ babel.config.js
@@ -1,9 +1,13 @@
-module.exports = function (api) {
-    api.cache(true);
-    return {
-      presets: [
-        ["babel-preset-expo", { jsxImportSource: "nativewind" }],
-        "nativewind/babel",
-      ],
-    };
+module.exports = {
+    presets: ["next/babel"],
+    plugins: [
+    [
+        "@babel/plugin-transform-react-jsx",
+       {
+         runtime: "automatic",
+         importSource: "nativewind",
+       },
+     ],
+   ],
   };
+  
