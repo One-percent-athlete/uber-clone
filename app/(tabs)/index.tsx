ca6c6c764d45f69b9ccaab276479a8159c694f2e
One-percent-athlete <one.percent.athlete@gmail.com>
2025-01-23 23:32:35 +0900

    Update index tsx

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,7 +1,8 @@
+import { ReactElement } from "react";
 import { View, Text, StatusBar } from 'react-native';
 import { SafeAreaView } from "react-native-safe-area-context";
 
-export default function HomeScreen() {
+export default function HomeScreen(): React.JSX.Element {
   return (
     <SafeAreaView className="text-red-500">
       <Text>Hello, World!</Text>
