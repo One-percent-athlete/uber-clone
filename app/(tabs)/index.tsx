bb7238695faaba32591a91efd4ce1d4e668eff9a
One-percent-athlete <one.percent.athlete@gmail.com>
2025-01-23 23:26:52 +0900

    Update index

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,10 +1,5 @@
 import { View, Text, Image, StyleSheet, Platform } from 'react-native';
 
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
-
 export default function HomeScreen() {
   return (
     <View className="text-red-500">
