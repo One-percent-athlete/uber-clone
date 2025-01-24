da79d3922da13ec737b8d88732e30937c83a5293
One-percent-athlete <one.percent.athlete@gmail.com>
2025-01-24 21:43:13 +0900

    Update auth layout file

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -1,16 +1,14 @@
-import { Stack } from 'expo-router';
-
+import { Stack } from "expo-router";
 
 const layout = () => {
-
   return (
-      <Stack>
-        <Stack.Screen name="index" options={{ headerShown: false }} />
-        <Stack.Screen name="(auth)" options={{ headerShown: false }} />
-        <Stack.Screen name="(root)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
+    <Stack>
+      <Stack.Screen name="index" options={{ headerShown: false }} />
+      <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+      <Stack.Screen name="(root)" options={{ headerShown: false }} />
+      <Stack.Screen name="+not-found" />
+    </Stack>
   );
-}
+};
+export default layout;
 
-export default layout
