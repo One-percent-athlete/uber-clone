92af6ec50256698caf9207d23bdcdaa54b4a1cd2
One-percent-athlete <one.percent.athlete@gmail.com>
2025-01-24 21:40:44 +0900

    Update auth layout

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -1,11 +1,7 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
-import { StatusBar } from 'expo-status-bar';
-import { ReactDOM, useEffect } from 'react';
-import 'react-native-reanimated';
-
+import { useEffect } from "react";
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
