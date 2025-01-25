8073a95c8d9f7e49f4d234ac00f5b0428e15ef94
One-percent-athlete <one.percent.athlete@gmail.com>
2025-01-25 23:15:47 +0900

    Update welcome page

--- app/(auth)/welcome.tsx
+++ app/(auth)/welcome.tsx
@@ -4,8 +4,13 @@ import { SafeAreaView, Text, Touchable, TouchableOpacity } from "react-native";
 const Onboarding = () => {
   return (
     <SafeAreaView className="flex w-full items-center justify-between bg-white">
-      <TouchableOpacity onPress={() => {router.replace("/(auth)/sign-up")}} className="w-full flex justify-end p-5">
-        <Text>Skip</Text>
+      <TouchableOpacity
+        onPress={() => {
+          router.replace("/(auth)/sign-up");
+        }}
+        className="w-full flex justify-end p-5"
+      >
+        <Text className="text-black text-md font-JakartaBold">Skip</Text>
       </TouchableOpacity>
     </SafeAreaView>
   );
