f4c46991cee6c97ccf034cc5924877ab393d7c2a
One-percent-athlete <one.percent.athlete@gmail.com>
2025-01-25 23:13:19 +0900

    Update welcome page

--- app/(auth)/welcome.tsx
+++ app/(auth)/welcome.tsx
@@ -1,9 +1,10 @@
+import { router } from "expo-router";
 import { SafeAreaView, Text, Touchable, TouchableOpacity } from "react-native";
 
 const Onboarding = () => {
   return (
     <SafeAreaView className="flex w-full items-center justify-between bg-white">
-      <TouchableOpacity>
+      <TouchableOpacity onPress={() => {router.replace("/(auth)/sign-up")}}>
         <Text>Skip</Text>
       </TouchableOpacity>
     </SafeAreaView>
