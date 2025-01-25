bc0ad0f1bbdb9e21082b75d671e9c8b174128c0e
One-percent-athlete <one.percent.athlete@gmail.com>
2025-01-25 22:59:14 +0900

    Update sign in page

---
+++ app/(auth)/sign-in.tsx
@@ -0,0 +1,10 @@
+import { SafeAreaView, Text } from "react-native";
+
+const Home = () => {
+  return (
+    <SafeAreaView>
+      <Text>Sign in</Text>
+    </SafeAreaView>
+  );
+};
+export default Home;
