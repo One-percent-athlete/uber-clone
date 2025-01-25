26c268548c1708e2991c44bba4dec6e8c055a64e
One-percent-athlete <one.percent.athlete@gmail.com>
2025-01-25 23:10:39 +0900

    Update welcome page

--- app/(auth)/welcome.tsx
+++ app/(auth)/welcome.tsx
@@ -2,7 +2,7 @@ import { SafeAreaView, Text } from "react-native";
 
 const Onboarding = () => {
   return (
-    <SafeAreaView>
+    <SafeAreaView className="flex w-full items-center justify-between bg-white">
       <Text>Onboarding</Text>
     </SafeAreaView>
   );
