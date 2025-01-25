502f44928708153516147201c840b2c0d7b2a611
One-percent-athlete <one.percent.athlete@gmail.com>
2025-01-25 23:03:17 +0900

    Update welcome page

---
+++ app/(auth)/wlecome.tsx
@@ -0,0 +1,10 @@
+import { SafeAreaView, Text } from "react-native";
+
+const Onboarding = () => {
+  return (
+    <SafeAreaView>
+      <Text>Onboarding</Text>
+    </SafeAreaView>
+  );
+};
+export default Onboarding;
