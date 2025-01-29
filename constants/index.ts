81ffcba4bf9b71a98ade82ebff13eb1fdea89237
One-percent-athlete <one.percent.athlete@gmail.com>
2025-01-29 23:15:33 +0900

    Update constants index ts file

--- constants/index.ts
+++ constants/index.ts
@@ -1,8 +1,10 @@
+import onboarding1 from '../assets/images/onboarding1.png';
+
 export const onboarding = [
   {
     id: 1,
     title: "Welcome to the app",
     description: "This is a description",
-    image: null,
+    image: onboarding1,
   },
 ]
