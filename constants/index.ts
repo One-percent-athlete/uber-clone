68918bf7de218d6a39aaaf651e1cea5b5225fc93
One-percent-athlete <one.percent.athlete@gmail.com>
2025-01-30 20:43:29 +0900

    Update index ts

--- constants/index.ts
+++ constants/index.ts
@@ -1,4 +1,7 @@
-import onboarding1 from '../assets/images/onboarding1.png';
+import onboarding1 from "../assets/images/onboarding1.png";
+import onboarding2 from "../assets/images/onboarding2.png";
+import onboarding3 from "../assets/images/onboarding3.png";
+import onboarding4 from "../assets/images/onboarding4.png";
 
 export const onboarding = [
   {
@@ -6,5 +9,23 @@ export const onboarding = [
     title: "Welcome to the app",
     description: "This is a description",
     image: onboarding1,
+  },
+  {
+    id: 2,
+    title: "Welcome to the app",
+    description: "This is a description",
+    image: onboarding2,
+  },
+  {
+    id: 3,
+    title: "Welcome to the app",
+    description: "This is a description",
+    image: onboarding3,
+  },
+  {
+    id: 4,
+    title: "Welcome to the app",
+    description: "This is a description",
+    image: onboarding4,
   },
 ]
