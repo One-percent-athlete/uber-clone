0cd282ff1ed2b71b5888c8af478b7287c0a31a2b
One-percent-athlete <one.percent.athlete@gmail.com>
2025-01-30 21:18:51 +0900

    Update welcome tsx

--- app/(auth)/welcome.tsx
+++ app/(auth)/welcome.tsx
@@ -1,14 +1,15 @@
 import { router } from "expo-router";
 import Swiper from "react-native-swiper";
-import { SafeAreaView, Text, TouchableOpacity } from "react-native";
-import { useRef } from "react";
+import { SafeAreaView, Text, TouchableOpacity, Image } from "react-native";
+import { useRef, useState } from "react";
 import { onboarding } from "@/constants";
 import { View } from "react-native-reanimated/lib/typescript/Animated";
 
 
 const Onboarding = () => {
   const swiperRef = useRef<Swiper>(null);
-  const const [activeIndex, setActiveIndex] = useState(0)
+  const [activeIndex, setActiveIndex] = useState(0);
+  
   return (
     <SafeAreaView className="flex w-full items-center justify-between bg-white">
       <TouchableOpacity
@@ -24,6 +25,7 @@ const Onboarding = () => {
 
       {onboarding.map((item) => (
         <View key={item.id} className="flex items-center justify-center p-5">
+          <Image source={item.image} style={{ width: '100%', height: 300 }} resizeMode="contain" />
           <Text>
             {item.title}
           </Text>
