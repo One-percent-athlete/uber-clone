582017c98a0061c864ef2a7a2c6745c78367e3e1
One-percent-athlete <one.percent.athlete@gmail.com>
2025-01-31 22:39:28 +0900

    Update custom button tsx file

--- assets/components/CustomButton.tsx
+++ assets/components/CustomButton.tsx
@@ -1,5 +1,9 @@
-const CustomButton = () => {
+import { TouchableOpacity } from "react-native";
 
-}
+const CustomButton = () => (
+    <TouchableOpacity>
+        <Text>Press me</Text>
+    </TouchableOpacity>
+)
 
 export default CustomButton;
