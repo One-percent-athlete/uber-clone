d4e7e98726b1fe306051b7926cb8b4cedffc5d75
One-percent-athlete <one.percent.athlete@gmail.com>
2025-02-01 22:34:44 +0900

    Update custom button tsx file

--- assets/components/CustomButton.tsx
+++ assets/components/CustomButton.tsx
@@ -16,6 +16,21 @@ const getBgVariantStyle = (variant: ButtonProps['bgVariant']) => {
   }
 }
 
+const getBgVariantStyle = (variant: ButtonProps['bgVariant']) => {
+  switch (variant) {
+    case "secondary":
+      return "bg-gray-500";
+    case "danger":
+      return "bg-red-500";
+    case "success":
+      return "bg-green-500";
+    case "outline":
+      return "bg-transparent borde-neutral-300 border-[0.5px]";
+    default:
+      return "bg-[#0286FF]";
+  }
+}
+
 const CustomButton = ({
   onPress,
   title,
