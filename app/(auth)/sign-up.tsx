81f2d53b268a7f561417eb12e3ffb3175461d6f8
One-percent-athlete <one.percent.athlete@gmail.com>
2025-02-07 22:46:20 +0900

    Update sign up tsx file

--- app/(auth)/sign-up.tsx
+++ app/(auth)/sign-up.tsx
@@ -1,9 +1,17 @@
 import { ScrollView, Text, View } from "react-native";
+import { images } from "@/constants";
 
 const SignUp = () => {
   return (
     <ScrollView className="flex-1 bg-white">
-      <View>Sign Up</View>
+      <View className="flex-1 bg-white">
+        <View>
+          <Image 
+            source={images.signUpCar} 
+            className="z-0 w-full h-[250px]"
+          />
+        </View>
+      </View>
     </ScrollView>
   );
 };
