3ef920d24237649214939c435488b24ac69f82df
One-percent-athlete <one.percent.athlete@gmail.com>
2025-02-07 22:32:02 +0900

    Update welcome tsx file

--- app/(auth)/welcome.tsx
+++ app/(auth)/welcome.tsx
@@ -49,7 +49,7 @@ const Onboarding = () => {
           </View>
         ))}
       </Swiper>
-      <CustomButton title={isLastSlide ? 'Get Started' : 'Next'} className="w-11/12 mt-10" />
+      <CustomButton title={isLastSlide ? 'Get Started' : 'Next'} onPress={() => isLastSlide ? router.replace('/(auth)/sign-up' : swiperRef.current ? scrollBy(1))} className="w-11/12 mt-10" />
     </SafeAreaView>
   );
 };
