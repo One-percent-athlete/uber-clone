823b5c3296025ba1cf6210f5ed7dda38908f69f1
One-percent-athlete <one.percent.athlete@gmail.com>
2025-02-10 23:00:49 +0900

    Update input field tsx

--- assets/components/InputField.tsx
+++ assets/components/InputField.tsx
@@ -1,12 +1,13 @@
 import React from 'react'
-import { KeyboardAvoidingView, TextInput } from "react-native"
+import {
+  KeyboardAvoidingView,
+  TextInput,
+  TouchableWithoutFeedback,
+} from "react-native";
 
 const InputField = () => (
   <KeyboardAvoidingView>
-    <TextInput
-      placeholder="Email"
-      style={{ height: 40, borderColor: "gray", borderWidth: 1 }}
-    />
+    <TouchableWithoutFeedback></TouchableWithoutFeedback>
   </KeyboardAvoidingView>
 )
 
