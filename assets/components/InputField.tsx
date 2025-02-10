4f68c622172559416331c6d3e8af7f21fe0e1841
One-percent-athlete <one.percent.athlete@gmail.com>
2025-02-10 22:59:12 +0900

    Update Input field html

--- assets/components/InputField.tsx
+++ assets/components/InputField.tsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import { KeyboardAvoidingView, TextInput } from "react-native"
 
-const InputField = () => {
-  return (
-    <div>InputField</div>
-  )
-}
+const InputField = () => (
+  <KeyboardAvoidingView>
+    <TextInput
+      placeholder="Email"
+      style={{ height: 40, borderColor: "gray", borderWidth: 1 }}
+    />
+  </KeyboardAvoidingView>
+)
 
 export default InputField
