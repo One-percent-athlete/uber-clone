09df25051e7881d165666676ce0915b7607edeb1
One-percent-athlete <one.percent.athlete@gmail.com>
2025-02-14 18:43:18 +0900

    Update sign up tsx

--- app/(auth)/sign-up.tsx
+++ app/(auth)/sign-up.tsx
@@ -22,10 +22,10 @@ const SignUp = () => {
         <View className="p-5">
           <InputField label="Name" placeholder="Enter Your Name" icon={icons.person} value={form.name}
               onChangeText={(value: any) => setForm({ ...form, name: value, })} labelStyle="" containStyle={undefined} inputStyle={undefined} iconStyle={undefined} className={undefined} />
-          <InputField label="Email" placeholder="Enter Your Email" icon={icons.person} value={form.name}
-              onChangeText={(value: any) => setForm({ ...form, name: value, })} labelStyle="" containStyle={undefined} inputStyle={undefined} iconStyle={undefined} className={undefined} />
-          <InputField label="Password" placeholder="Enter Your Password" icon={icons.person} value={form.name}
-              onChangeText={(value: any) => setForm({ ...form, name: value, })} labelStyle="" containStyle={undefined} inputStyle={undefined} iconStyle={undefined} className={undefined} />
+          <InputField label="Email" placeholder="Enter Your Email" icon={icons.email} value={form.email}
+              onChangeText={(value: any) => setForm({ ...form, email: value, })} labelStyle="" containStyle={undefined} inputStyle={undefined} iconStyle={undefined} className={undefined} />
+          <InputField label="Password" placeholder="Enter Your Password" icon={icons.lock} value={form.password}
+              onChangeText={(value: any) => setForm({ ...form, password: value, })} labelStyle="" containStyle={undefined} inputStyle={undefined} iconStyle={undefined} className={undefined} />
         </View>
       </View>
     </ScrollView>
