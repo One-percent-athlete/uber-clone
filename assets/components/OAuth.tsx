f7c9bb47005d14b159d63226298aff638322f7d4
One-percent-athlete <one.percent.athlete@gmail.com>
2025-02-15 22:48:42 +0900

    Update oauth html

--- assets/components/OAuth.tsx
+++ assets/components/OAuth.tsx
@@ -1,1 +1,12 @@
-const OAuth = () => {}
+import { View } from "react-native";
+
+
+const OAuth = () => (
+  <View>
+    <View>
+      <Text>OAuth</Text>
+    </View>
+  </View>
+)
+
+export default OAuth;
