885eb08d0fcc7de4bb7b51c6e681414140c3029f
One-percent-athlete <one.percent.athlete@gmail.com>
2025-02-15 22:52:57 +0900

    Update o auth tsx

--- assets/components/OAuth.tsx
+++ assets/components/OAuth.tsx
@@ -5,7 +5,8 @@ const OAuth = () => (
   <View>
     <View className="flex flex-row items-center justify-center mt-4 gap-x-3">
       <View className="flex-1 h-[1px] bg-general-100" />
-      <Text></Text>
+      <Text className="text-lg">Or</Text>
+      <View className="flex-1 h-[1px] bg-general-100" />
     </View>
   </View>
 )
