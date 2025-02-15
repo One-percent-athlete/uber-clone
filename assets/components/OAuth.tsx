5cba417b42390e5f3a08c8cf373878d53efa0443
One-percent-athlete <one.percent.athlete@gmail.com>
2025-02-15 22:51:48 +0900

    Update o auth tsx

--- assets/components/OAuth.tsx
+++ assets/components/OAuth.tsx
@@ -1,10 +1,11 @@
-import { View } from "react-native";
+import { View, Text } from "react-native";
 
 
 const OAuth = () => (
   <View>
     <View className="flex flex-row items-center justify-center mt-4 gap-x-3">
-      <View />
+      <View className="flex-1 h-[1px] bg-general-100" />
+      <Text></Text>
     </View>
   </View>
 )
