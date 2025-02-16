b5d7004153b49c2e8b203f7cb20f1becfe956797
One-percent-athlete <one.percent.athlete@gmail.com>
2025-02-16 22:26:53 +0900

    Update sign in tsx

--- app/(auth)/sign-in.tsx
+++ app/(auth)/sign-in.tsx
@@ -22,7 +22,7 @@ const SignIn = () => {
       <View className="flex-1 bg-white">
         <View className="relative w-full h-[250px]">
           <Image source={images.signUpCar} className="z-0 w-full h-[250px]" />
-          <Text className="text-2xl text-black font-JakartaBold absolute buttom-5 left-5">Create Your Account<Text>
+          <Text className="text-2xl text-black font-JakartaBold absolute buttom-5 left-5">Welcome to <Text>
         </View>
 
         <View className="p-5">
