0056a663057f64bdb0d67d009410fde7a69c56a6
One-percent-athlete <one.percent.athlete@gmail.com>
2025-02-16 22:24:45 +0900

    Update sign in tsx

--- app/(auth)/sign-in.tsx
+++ app/(auth)/sign-in.tsx
@@ -1,10 +1,49 @@
-import { SafeAreaView, Text } from "react-native";
+import { ScrollView, Text, View } from "react-native";
+import { icons, images } from "@/constants";
+import InputField from "@/assets/components/InputField";
+import { useState } from "react";
+import CustomButton from "@/assets/components/CustomButton";
+import OAuth from "@/assets/components/OAuth";
 
-const SignIn = () => {
+const SignUp = () => {
+
+  const [form, setForm] = useState({
+    name: "",
+    email:"",
+    password: "",
+  })
+
+  const onSignUpPress = async () => {
+    console.log(form);
+  }
+
   return (
-    <SafeAreaView>
-      <Text>Sign In</Text>
-    </SafeAreaView>
+    <ScrollView className="flex-1 bg-white">
+      <View className="flex-1 bg-white">
+        <View className="relative w-full h-[250px]">
+          <Image source={images.signUpCar} className="z-0 w-full h-[250px]" />
+          <Text className="text-2xl text-black font-JakartaBold absolute buttom-5 left-5">Create Your Account<Text>
+        </View>
+
+        <View className="p-5">
+          <InputField label="Name" placeholder="Enter Your Name" icon={icons.person} value={form.name}
+              onChangeText={(value: any) => setForm({ ...form, name: value, })} labelStyle="" containStyle={undefined} inputStyle={undefined} iconStyle={undefined} className={undefined} />
+          <InputField label="Email" placeholder="Enter Your Email" icon={icons.email} value={form.email}
+              onChangeText={(value: any) => setForm({ ...form, email: value, })} labelStyle="" containStyle={undefined} inputStyle={undefined} iconStyle={undefined} className={undefined} />
+          <InputField label="Password" placeholder="Enter Your Password" icon={icons.lock} secureTextEntry={true} value={form.password}
+              onChangeText={(value: any) => setForm({ ...form, password: value, })} labelStyle="" containStyle={undefined} inputStyle={undefined} iconStyle={undefined} className={undefined} />
+          <CustomButton title="Sign Up" onPress={onSignUpPress} className="mt-6"/>
+
+          <OAuth />
+
+          <Link href="/sign-in" className="text-lg text-center text-general-200 mt-10">
+            <Text>Already have an account?</Text>
+            <Text className="text-primary-500">Login</Text>
+          </Link>
+        </View>
+      </View>
+    </ScrollView>
   );
 };
-export default SignIn;
+
+export default SignUp;
