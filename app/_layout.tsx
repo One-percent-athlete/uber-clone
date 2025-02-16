c2b55752491f1501a64f0f2dd01fe8c9494400a3
One-percent-athlete <one.percent.athlete@gmail.com>
2025-02-16 22:45:11 +0900

    Update root _layout tsx

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -5,6 +5,24 @@ import * as SplashScreen from 'expo-splash-screen';
 import { StatusBar } from 'expo-status-bar';
 import { ReactDOM, useEffect } from 'react';
 import 'react-native-reanimated';
+import { ClerkProvider, ClerkLoaded } from '@clerk/clerk-expo'
+import { Slot } from 'expo-router'
+
+export default function RootLayout() {
+  const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!
+
+  if (!publishableKey) {
+    throw new Error('Add EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY to your .env file')
+  }
+
+  return (
+    <ClerkProvider publishableKey={publishableKey}>
+      <ClerkLoaded>
+        <Slot />
+      </ClerkLoaded>
+    </ClerkProvider>
+  )
+}
 
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
