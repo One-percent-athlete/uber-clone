f0c91c5d50cb2cb5610dbc704a6dc33a4ffe734b
One-percent-athlete <one.percent.athlete@gmail.com>
2025-02-17 22:17:43 +0900

    Update sign up tsx

--- app/(auth)/sign-up.tsx
+++ app/(auth)/sign-up.tsx
@@ -4,8 +4,11 @@ import InputField from "@/assets/components/InputField";
 import { useState } from "react";
 import CustomButton from "@/assets/components/CustomButton";
 import OAuth from "@/assets/components/OAuth";
+import { useSignUp } from "@clerk/clerk-expo";
 
 const SignUp = () => {
+
+  const { isLoaded, signUp, setActive } = useSignUp();
 
   const [form, setForm] = useState({
     name: "",
@@ -19,28 +22,26 @@ const SignUp = () => {
 
   return (
     <ScrollView className="flex-1 bg-white">
-      <View className="flex-1 bg-white">
-        <View className="relative w-full h-[250px]">
-          <Image source={images.signUpCar} className="z-0 w-full h-[250px]" />
-          <Text className="text-2xl text-black font-JakartaBold absolute buttom-5 left-5">Create Your Account<Text>
-        </View>
+      <View className="relative w-full h-[250px]">
+        <Image source={images.signUpCar} className="z-0 w-full h-[250px]" />
+        <Text className="text-2xl text-black font-JakartaBold absolute buttom-5 left-5">Create Your Account<Text>
+      </View>
 
-        <View className="p-5">
-          <InputField label="Name" placeholder="Enter Your Name" icon={icons.person} value={form.name}
-              onChangeText={(value: any) => setForm({ ...form, name: value, })} labelStyle="" containStyle={undefined} inputStyle={undefined} iconStyle={undefined} className={undefined} />
-          <InputField label="Email" placeholder="Enter Your Email" icon={icons.email} value={form.email}
-              onChangeText={(value: any) => setForm({ ...form, email: value, })} labelStyle="" containStyle={undefined} inputStyle={undefined} iconStyle={undefined} className={undefined} />
-          <InputField label="Password" placeholder="Enter Your Password" icon={icons.lock} secureTextEntry={true} value={form.password}
-              onChangeText={(value: any) => setForm({ ...form, password: value, })} labelStyle="" containStyle={undefined} inputStyle={undefined} iconStyle={undefined} className={undefined} />
-          <CustomButton title="Sign Up" onPress={onSignUpPress} className="mt-6"/>
+      <View className="p-5">
+        <InputField label="Name" placeholder="Enter Your Name" icon={icons.person} value={form.name}
+            onChangeText={(value: any) => setForm({ ...form, name: value, })} labelStyle="" containStyle={undefined} inputStyle={undefined} iconStyle={undefined} className={undefined} />
+        <InputField label="Email" placeholder="Enter Your Email" icon={icons.email} value={form.email}
+            onChangeText={(value: any) => setForm({ ...form, email: value, })} labelStyle="" containStyle={undefined} inputStyle={undefined} iconStyle={undefined} className={undefined} />
+        <InputField label="Password" placeholder="Enter Your Password" icon={icons.lock} secureTextEntry={true} value={form.password}
+            onChangeText={(value: any) => setForm({ ...form, password: value, })} labelStyle="" containStyle={undefined} inputStyle={undefined} iconStyle={undefined} className={undefined} />
+        <CustomButton title="Sign Up" onPress={onSignUpPress} className="mt-6"/>
 
-          <OAuth />
+        <OAuth />
 
-          <Link href="/sign-in" className="text-lg text-center text-general-200 mt-10">
-            <Text>Already have an account?</Text>
-            <Text className="text-primary-500">Login</Text>
-          </Link>
-        </View>
+        <Link href="/sign-in" className="text-lg text-center text-general-200 mt-10">
+          <Text>Already have an account?</Text>
+          <Text className="text-primary-500">Login</Text>
+        </Link>
       </View>
     </ScrollView>
   );
