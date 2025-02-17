8a208a833b5d01dda2c2bef2dfe670fd43de3144
One-percent-athlete <one.percent.athlete@gmail.com>
2025-02-17 22:10:41 +0900

    Update home and index tsx

--- app/(root)/(tabs)/home.tsx
+++ app/(root)/(tabs)/home.tsx
@@ -1,6 +1,6 @@
-import { SignedIn, SignedOut, useUser } from '@clerk/clerk-expo'
-import { Link } from 'expo-router'
-import { Text, View } from 'react-native'
+import { SignedIn, SignedOut, useUser } from "@clerk/clerk-expo";
+import { Link } from "expo-router";
+import { Text, View } from "react-native";
 
 export default function Page() {
   const { user } = useUser()
--- app/index.tsx
+++ app/index.tsx
@@ -3,6 +3,10 @@ import { View, Text } from "react-native"
 import { SafeAreaView } from "react-native-safe-area-context";
 
 const Home = () => {
+  
+  if (isSignedIn) {
+    return <Redirect href={'/'} />
+  }
   return <Redirect href="/(auth)/welcome" />
 }
 
