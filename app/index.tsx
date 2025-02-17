b64508e750e927a61954aeee550a52ffbb1f67fa
One-percent-athlete <one.percent.athlete@gmail.com>
2025-02-17 22:12:03 +0900

    Update index tsx

--- app/index.tsx
+++ app/index.tsx
@@ -1,8 +1,11 @@
+import { useAuth } from "@clerk/clerk-expo";
 import { Redirect } from "expo-router";
 import { View, Text } from "react-native"
 import { SafeAreaView } from "react-native-safe-area-context";
 
 const Home = () => {
+
+  const { isSignedIn } = useAuth()
   
   if (isSignedIn) {
     return <Redirect href={'/'} />
