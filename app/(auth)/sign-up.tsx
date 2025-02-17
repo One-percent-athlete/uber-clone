d02dd694bb09388b01b4f4d5e403a95c272a8962
One-percent-athlete <one.percent.athlete@gmail.com>
2025-02-17 22:28:43 +0900

    Update sign up tsx

--- app/(auth)/sign-up.tsx
+++ app/(auth)/sign-up.tsx
@@ -42,6 +42,31 @@ const SignUp = () => {
     } catch (err) {
       console.error(JSON.stringify(err, null, 2))
     }
+
+    const onVerifyPress = async () => {
+      if (!isLoaded) return
+  
+      try {
+        // Use the code the user provided to attempt verification
+        const signUpAttempt = await signUp.attemptEmailAddressVerification({
+          code,
+        })
+  
+        // If verification was completed, set the session to active
+        // and redirect the user
+        if (signUpAttempt.status === 'complete') {
+          await setActive({ session: signUpAttempt.createdSessionId })
+          router.replace('/')
+        } else {
+          // If the status is not complete, check why. User may need to
+          // complete further steps.
+          console.error(JSON.stringify(signUpAttempt, null, 2))
+        }
+      } catch (err) {
+        // See https://clerk.com/docs/custom-flows/error-handling
+        // for more info on error handling
+        console.error(JSON.stringify(err, null, 2))
+      }
   }
 
   return (
