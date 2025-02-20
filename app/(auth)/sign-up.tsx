7f7eb631ac59e2a10fb3f7c154190b658d3e503a
One-percent-athlete <one.percent.athlete@gmail.com>
2025-02-20 21:22:17 +0900

    Update sign up tsx

--- app/(auth)/sign-up.tsx
+++ app/(auth)/sign-up.tsx
@@ -12,12 +12,12 @@ const SignUp = () => {
 
   const [form, setForm] = useState({
     name: "",
-    email:"",
+    email: "",
     password: "",
   })
 
   const [verification, setVerification] = useState({
-    state: "default",
+    state: "success",
     error: "",
     code: ""
   });
@@ -25,15 +25,12 @@ const SignUp = () => {
     if (!isLoaded) {
       return
     }
-
     try {
       await signUp.create({
         emailAddress: form.email,
         password: form.password,
       })
-
       await signUp.prepareEmailAddressVerification({ strategy: "email_code" });
-
       setVerification({
         ...verification,
         state: "pending",
@@ -72,7 +69,6 @@ const SignUp = () => {
   return (
     <ScrollView className="flex-1 bg-white">
       <View className="relative w-full h-[250px]">
-        
         <Image source={images.signUpCar} className="z-0 w-full h-[250px]" />
         <Text className="text-2xl text-black font-JakartaBold absolute buttom-5 left-5">
           Create Your Account
