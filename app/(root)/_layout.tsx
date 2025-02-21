7da000097fb53711d369d49738265c528cac2b50
One-percent-athlete <one.percent.athlete@gmail.com>
2025-02-21 22:33:23 +0900

    Update root layout tsx

--- app/(root)/_layout.tsx
+++ app/(root)/_layout.tsx
@@ -3,11 +3,8 @@ import { Stack } from "expo-router";
 const layout = () => {
   return (
     <Stack>
-      <Stack.Screen name="welcome" options={{ headerShown: false }} />
-      <Stack.Screen name="sign-up" options={{ headerShown: false }} />
-      <Stack.Screen name="sign-in" options={{ headerShown: false }} />
+      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
     </Stack>
   );
 };
 export default layout;
-
