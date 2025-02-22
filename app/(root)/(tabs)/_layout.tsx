806b646b71858111e957252bf733ed6026408ffc
One-percent-athlete <one.percent.athlete@gmail.com>
2025-02-22 22:50:34 +0900

    Update tabs layout tsx

--- app/(root)/(tabs)/_layout.tsx
+++ app/(root)/(tabs)/_layout.tsx
@@ -1,7 +1,12 @@
 import { Tabs } from "expo-router";
 
-const layout = () => (
-    <Tabs>
-        
-    </Tabs>
+const layout = () =>(
+  <Tabs
+    initialRouteName="index"
+    screenOptions={{
+      tabBarActiveTintColor = "white",
+    }}
+    ></Tabs>
 )
+
+export default layout;
