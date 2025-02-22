94408560063733359f6249a472371e3ff9d8092f
One-percent-athlete <one.percent.athlete@gmail.com>
2025-02-22 23:08:40 +0900

    Update tabs layout tsx

--- app/(root)/(tabs)/_layout.tsx
+++ app/(root)/(tabs)/_layout.tsx
@@ -27,6 +27,26 @@ const layout = () =>(
     }} 
     />
     <Tabs.Screen
+      name="home"
+      options={{
+        title: "Home",
+        headerShown: false,
+        tabBarIcon: ({ focused }) => (
+          <TabIcon focused={focused} source={icons.home} />
+        ),
+    }} 
+    />
+    <Tabs.Screen
+      name="rides"
+      options={{
+        title: "Rides",
+        headerShown: false,
+        tabBarIcon: ({ focused }) => (
+          <TabIcon focused={focused} source={icons.list} />
+        ),
+    }} 
+    />
+    <Tabs.Screen
       name="profile"
       options={{
         title: "Profile",
