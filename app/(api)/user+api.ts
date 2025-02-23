c00da23c58a64ee32aa5b8b45f28dcf2916a6f42
One-percent-athlete <one.percent.athlete@gmail.com>
2025-02-23 22:30:03 +0900

    Update user api ts

--- app/(api)/user+api.ts
+++ app/(api)/user+api.ts
@@ -3,7 +3,14 @@ import { neon } from "@neondatabase/serverless";
 
 export async function POST(request: Request) {
   const sql = neon(`{process.env.YOUR_DATABASE_URL}`);
-  const {name, email, clerkId } = await request.json()
+  const {name, email, clerkId } = await request.json();
+
+  if (!name || !email || !clerkId) {
+    return new Response.json(
+      { error: "Missing required fields" },
+      { status: 400 },
+    );
+  }
   try {
   } catch (error) {}
 }
