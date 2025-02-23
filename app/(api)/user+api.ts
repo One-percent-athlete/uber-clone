93ee74a929f4396be74000cee5f97b7535dcf391
One-percent-athlete <one.percent.athlete@gmail.com>
2025-02-23 22:25:16 +0900

    Update user api ts

--- app/(api)/user+api.ts
+++ app/(api)/user+api.ts
@@ -3,6 +3,7 @@ import { neon } from "@neondatabase/serverless";
 
 export async function POST(request: Request) {
   const sql = neon(`{process.env.YOUR_DATABASE_URL}`);
+  const {name, email, clerkId } = await request.json()
   try {
   } catch (error) {}
 }
