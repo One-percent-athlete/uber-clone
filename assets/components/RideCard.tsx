6f0445962911c88d71eeec9cb2c2cf663c382f3e
One-percent-athlete <one.percent.athlete@gmail.com>
2025-02-24 22:18:46 +0900

    Update Ride card tsx

--- assets/components/RideCard.tsx
+++ assets/components/RideCard.tsx
@@ -1,9 +1,22 @@
 import { Ride } from "@/types/type";
 import { View, Text } from "react-native";
 
-const RideCard = ({ ride }: { ride: Ride }) => (
+const RideCard = ({
+  ride: {
+    destination_longitude,
+    destination_latitude,
+    origin_address,
+    destination_address,
+    created_at,
+    ride_time,
+    driver,
+    payment_status,
+  },
+}: {
+  ride: Ride;
+}) => (
   <View>
-    <Text>{ride.driver.first_name}</Text>
+    <Text className="text-3xl">{ride.driver.first_name}</Text>
   </View>
 )
 
