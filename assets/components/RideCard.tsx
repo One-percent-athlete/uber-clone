cf24d33265168e00bc775d297d90b4a32397170a
One-percent-athlete <one.percent.athlete@gmail.com>
2025-02-25 22:44:03 +0900

    Update ride card tsx

--- assets/components/RideCard.tsx
+++ assets/components/RideCard.tsx
@@ -18,30 +18,30 @@ const RideCard = ({
 }) => (
   <View className="flex flex-row items-center justify-center bg-white rounded-lg shadow-sm shadow-neutral-300 mb-3">
     <View className="flex flex-row items-center justify-between p-3">
-        <View className="flex flex-row items-center justify-between">
+      <View className="flex flex-row items-center justify-between">
+        <Image
+            source={{ uri: `https://maps.geoapify.com/v1/staticmap?style=osm-bright&width=600&height=400&center=lonlat:${destination_longitude},${destination_latitude}&zoom=14&apiKey=${process.env.EXPO_PUBLIC_GEOAPIFY_API_KEY}` }}
+            className="w-[80px] h-[90px] rounded-lg"
+        />
+        <View className="flex flex-col mx-5 gap-y-5 flex-1">
+          <View className="flex flex-row items-center gap-x-2">
             <Image
-                source={{ uri: `https://maps.geoapify.com/v1/staticmap?style=osm-bright&width=600&height=400&center=lonlat:${destination_longitude},${destination_latitude}&zoom=14&apiKey=${process.env.EXPO_PUBLIC_GEOAPIFY_API_KEY}` }}
-                className="w-[80px] h-[90px] rounded-lg"
+                source={icons.to}
+                className="w-5 h-5"
             />
-            <View className="flex flex-col mx-5 gap-y-5 flex-1">
-                <View className="flex flex-row items-center gap-x-2">
-                    <Image
-                        source={icons.to}
-                        className="w-5 h-5"
-                    />
-                    <Text className="text-md font-JakartaMedium" numberOfLines={1}>{origin_address}</Text>
-                </View>
-                
-                <View className="flex flex-row items-center gap-x-2">
-                    <Image
-                        source={icons.point}
-                        className="w-5 h-5"
-                    />
-                    <Text className="text-md font-JakartaMedium" numberOfLines={1}>{destination_address}</Text>
-                </View>
-            </View>
+            <Text className="text-md font-JakartaMedium" numberOfLines={1}>{origin_address}</Text>
+          </View>
+            
+          <View className="flex flex-row items-center gap-x-2">
+              <Image
+                source={icons.point}
+                className="w-5 h-5"
+              />
+              <Text className="text-md font-JakartaMedium" numberOfLines={1}>{destination_address}</Text>
+          </View>
+        </View>
+      </View>
     </View>
-    <Text className="text-3xl">{driver.first_name}</Text>
   </View>
 )
 
