eb61a9144bf4494b40c897b0792f14197f2aa73c
One-percent-athlete <one.percent.athlete@gmail.com>
2025-02-25 22:22:29 +0900

    Update ride card tsx

--- assets/components/RideCard.tsx
+++ assets/components/RideCard.tsx
@@ -16,6 +16,17 @@ const RideCard = ({
   ride: Ride;
 }) => (
   <View className="flex flex-row items-center justify-center bg-white rounded-lg shadow-sm shadow-neutral-300 mb-3">
+    <View className="flex flex-row items-center justify-between p-3">
+        <View className="flex flex-row items-center justify-between">
+            <Image
+                className="w-12 h-12 rounded-full"
+                source={{ uri: driver.profile_image_url }}
+            />
+            <View className="ml-3">
+                <Text className="text-sm text-general-200">Driver</Text>
+                <Text className="text-lg font-JakartaExtraBold">{driver.first_name}</Text>
+            </View>
+    </View>
     <Text className="text-3xl">{driver.first_name}</Text>
   </View>
 )
