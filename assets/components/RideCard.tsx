0cf377072231d8eaae3786587f1f81ce7e3ca147
One-percent-athlete <one.percent.athlete@gmail.com>
2025-02-27 20:03:03 +0900

    Update ride card tsx

--- assets/components/RideCard.tsx
+++ assets/components/RideCard.tsx
@@ -18,7 +18,7 @@ const RideCard = ({
   ride: Ride;
 }) => (
   <View className="flex flex-row items-center justify-center bg-white rounded-lg shadow-sm shadow-neutral-300 mb-3">
-    <View className="flex flex-col items-center justify-between p-3">
+    <View className="flex flex-col items-center justify-center p-3">
       <View className="flex flex-row items-center justify-between">
         <Image
             source={{ uri: `https://maps.geoapify.com/v1/staticmap?style=osm-bright&width=600&height=400&center=lonlat:${destination_longitude},${destination_latitude}&zoom=14&apiKey=${process.env.EXPO_PUBLIC_GEOAPIFY_API_KEY}` }}
