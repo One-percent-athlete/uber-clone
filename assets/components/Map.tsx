bb675dfe570e8f6abce031fcd4721795c69c47b8
One-percent-athlete <one.percent.athlete@gmail.com>
2025-02-28 17:50:42 +0900

    Create map component

--- assets/components/Map.tsx
+++ assets/components/Map.tsx
@@ -1,3 +1,10 @@
-const Map = () => {}
+import { View, Text } from "react-native";
 
+const Map = () => {
+  return (
+    <View className="flex-1 bg-red-500">
+      <Text className="text-white">Map</Text>
+    </View>
+);
+}
 export default Map
