5bbaa134b279eb7c5b1fd6d503bc71ce507733c3
One-percent-athlete <one.percent.athlete@gmail.com>
2025-03-01 21:25:16 +0900

    Update index ts

--- store/index.ts
+++ store/index.ts
@@ -2,6 +2,10 @@ import { LocationStore } from "@/types/type";
 import { create } from 'zustand';
 
 export const useLocationStore = create<LocationStore>((set) => ({
-
-})
-)
+    userAddress: null,
+    userLongitude: null,
+    userAddress: null,
+    destinationLongitude: null,
+    destinationLatitude: null,
+　})
+);
