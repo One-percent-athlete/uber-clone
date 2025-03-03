92f39c73328a6f1c191ea1b33a517ef3afc9a652
One-percent-athlete <one.percent.athlete@gmail.com>
2025-03-03 23:16:15 +0900

    Update index ts

--- store/index.ts
+++ store/index.ts
@@ -1,19 +1,26 @@
 import { LocationStore } from "@/types/type";
-import { create } from 'zustand';
+import { create } from "zustand";
 
 export const useLocationStore = create<LocationStore>((set) => ({
-    userAddress: null,
-    userLongitude: null,
-    userAddress: null,
-    destinationLongitude: null,
-    destinationLatitude: null,
-    destinationAddress: null,
-    setUserLocation: ({ latitude, longitude, address }: { latitude: number, longitude: number, address: string }) => {
-        set(() => ({
-            destinationLatitude: latitude,
-            destinationLongitude: longitude,
-            destinationAddress: address
-        }))
-    },
-
+  userAddress: null,
+  userLongitude: null,
+  userAddress: null,
+  destinationLongitude: null,
+  destinationLatitude: null,
+  destinationAddress: null,
+  setUserLocation: ({
+    latitude,
+    longitude,
+    address,
+  }: {
+    latitude: number;
+    longitude: number;
+    address: string;
+  }) => {
+    set(() => ({
+      destinationLatitude: latitude,
+      destinationLongitude: longitude,
+      destinationAddress: address,
+    }));
+  },
 }));
