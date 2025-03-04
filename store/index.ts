85da2618fe67540a2889240a6fb5e9922217024d
One-percent-athlete <one.percent.athlete@gmail.com>
2025-03-04 23:11:07 +0900

    Update index ts

--- store/index.ts
+++ store/index.ts
@@ -43,5 +43,9 @@ export const useLocationStore = create<LocationStore>((set) => ({
 export const useDriverStore = create<DriverStore>((set) => ({
   drivers: [] as MarkerData[],
   selectedDriver: null,
-  setSelectedDriver: (driverId: null) => set(() => ({ selectedDriver: driverId })),
+  setSelectedDriver: (driverId: number) =>
+    set(() => ({ selectedDriver: driverId })),
+  setDrivers: (drivers: MarkerData[]) => set(() => ({ drivers: drivers })),
+  clearSelectedDriver: () => set(() => ({ selectedDriver: null })),
+
 }));
