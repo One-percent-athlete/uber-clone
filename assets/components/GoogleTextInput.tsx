c29d9c041a96f2753d7402ebc7c40c9c2987c849
One-percent-athlete <one.percent.athlete@gmail.com>
2025-03-05 16:43:11 +0900

    Update google text input tsx

--- assets/components/GoogleTextInput.tsx
+++ assets/components/GoogleTextInput.tsx
@@ -12,7 +12,21 @@ const GoogleTextInput = ({
   <View
     className={`flex flex-row items-center justify-center relative z-50 rounded-xl ${containerStyle} mb-5`}
   >
-    <GooglePlacesAutocomplete />
+    <GooglePlacesAutocomplete 
+     fetchDetails={true}
+      placeholder="Where do you want to go?"
+      debounce={200}
+      styles={{
+        textInputContainer: {
+          alignItems: "center",
+          justifyContent: "center",
+          borderRadius: 20,
+          marginHorizontal: 20,
+          position: "relative",
+          shadowColor: "#d4d4d4",
+        }
+      }}
+    />
   </View>
 );
 
