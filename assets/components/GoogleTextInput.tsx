b98d6b1ebf1deae3897791c261ca32858277b113
One-percent-athlete <one.percent.athlete@gmail.com>
2025-03-05 16:57:45 +0900

    Update google text input tsx

--- assets/components/GoogleTextInput.tsx
+++ assets/components/GoogleTextInput.tsx
@@ -1,7 +1,9 @@
-import { data } from "@/constants";
+import { data, icons } from "@/constants";
 import { GoogleInputProps } from "@/types/type";
 import { Text, View } from "react-native";
 import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
+
+const googleplacesApiKey = process.env.EXPO_PUBLIC_GOOGLE_API_KEY;
 
 const GoogleTextInput = ({
   icon,
@@ -26,7 +28,7 @@ const GoogleTextInput = ({
           position: "relative",
           shadowColor: "#d4d4d4",
         },
-        textInput:{
+        textInput: {
           backgroundColor: textInputBackgroundColor || "white",
           fontSize: "16",
           fontWeight: "600",
@@ -42,15 +44,28 @@ const GoogleTextInput = ({
           borderRadius: 10,
           shadowColor: "#d4d4d4",
           zIndex: 99,
-        }
+        },
       }}
       onPress={(data, details = null) => {
         handlePress({
           latitude: details?.geometry.location.lat!,
           longitude: details?.geometry.location.lng!,
           address: data.description,
-        })
+        });
       }}
+      query={{
+        key: googleplacesApiKey,
+        language: "en",
+      }}
+      renderLeftButton={() => (
+        <View className="justtiy-center items-center w-6 h-6">
+          <Image
+            source={icon ? icon : icons.search}
+            className="w-6 h-6"
+            resizeMode="contain"
+          />
+        </View>
+      )}
     />
   </View>
 );
