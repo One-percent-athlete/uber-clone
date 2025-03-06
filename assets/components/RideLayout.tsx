4e86c7211d01e1410bb30b5e8b0d36f5c911cd9d
One-percent-athlete <one.percent.athlete@gmail.com>
2025-03-06 21:07:32 +0900

    Update ride layout tsx

--- assets/components/RideLayout.tsx
+++ assets/components/RideLayout.tsx
@@ -1,8 +1,12 @@
-const RideLayout = () => {
+import { View, Text } from "react-native";
+
+const RideLayout = ({children}: {children: React.ReactNode}) => {
   return (
-    <Layout>
-      <FindRide />
-    </Layout>
+    <View className="flex flex-col h-full">
+      <Text>TOP OF THE LAYOUT</Text>
+      {children}
+      <Text>BUTTOM OF THE LAYOUT</Text>
+    </View>
   );
 }
 
