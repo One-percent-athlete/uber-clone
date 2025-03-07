8ccb4cf4badf7cc1cebc74cb98924704328bd983
One-percent-athlete <one.percent.athlete@gmail.com>
2025-03-07 23:51:57 +0900

    Update find ride tsx

--- app/(root)/find-ride.tsx
+++ app/(root)/find-ride.tsx
@@ -1,19 +1,30 @@
+import GoogleTextInput from "@/assets/components/GoogleTextInput";
 import RideLayout from "@/assets/components/RideLayout";
+import { icons } from "@/constants";
 import { useLocationStore } from "@/store";
 import { View, Text } from "react-native";
 
 const FindRide = () => {
-  const { userAddress, destinationAddress, setDestinationLocation } =
-    useLocationStore();
+  const {
+    userAddress,
+    destinationAddress,
+    setDestinationLocation,
+    setUserLocation,
+  } = useLocationStore();
   return (
-    <RideLayout>
-      <Text className="text-2xl">Find Ride</Text>
-      <Text className="text-2xl">Find Ride</Text>
-      <Text className="text-2xl">Find Ride</Text>
-      <Text className="text-2xl">Find Ride</Text>
-      <Text className="text-2xl">Find Ride</Text>
+    <RideLayout title="Ride">
+      <View className="my-3">
+        <Text className="text-2xl font-JakartaSemiBold mb-3">From</Text>
+        <GoogleTextInput
+          icon={icons.target}
+          initialLocation={userAddress}
+          containerStyle="bg-neutral-100"
+          textInputBackgroundColor="#f5f5f5"
+          handlePress={(location) => setUserLocation(location)}
+        />
+      </View>
     </RideLayout>
   );
-}
+};
 
 export default FindRide;
