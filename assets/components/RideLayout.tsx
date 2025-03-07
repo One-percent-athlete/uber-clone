85f5637dd3eff330bb4faaeb121f17ad986b1a05
One-percent-athlete <one.percent.athlete@gmail.com>
2025-03-07 20:15:17 +0900

    Update ride layout tsx

--- assets/components/RideLayout.tsx
+++ assets/components/RideLayout.tsx
@@ -3,8 +3,14 @@ import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 const RideLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <GestureHandlerRootView className="flex flex-col h-full">
-      {children}
+    <GestureHandlerRootView>
+      <View className="flex-1 bg-white">
+        <View className="flex flex-col h-screen bg-blue-500">
+          <View className="flex flex-row absolute z-10 top-16 items-center justify-start px-5">
+            
+          </View>
+        </View>
+      </View>
     </GestureHandlerRootView>
   );
 };
