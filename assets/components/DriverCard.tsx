272f0b584e3e5c367a7925a3cba580e633681962
One-percent-athlete <one.percent.athlete@gmail.com>
2025-03-08 22:26:12 +0900

    Update driver card tsx

--- assets/components/DriverCard.tsx
+++ assets/components/DriverCard.tsx
@@ -1,5 +1,11 @@
+import React from "react";
+import { View } from "react-native";
 
 
-const DriverCard = ({ itme } : RiderCardProps) => ()
+const DriverCard = ({ itme } : RiderCardProps) => (
+    <View>
+        <Text></Text>
+    </View>
+)
 
 export default DriverCard;
