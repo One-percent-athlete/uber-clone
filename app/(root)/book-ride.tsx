61840176919e522b65f2dff2d18519fa2ffaa003
One-percent-athlete <one.percent.athlete@gmail.com>
2025-03-10 22:01:44 +0900

    Update book ride tsx

--- app/(root)/book-ride.tsx
+++ app/(root)/book-ride.tsx
@@ -56,7 +56,7 @@ const BookRide = () => {
           <View className="flex flex-row items-center justify-between w-full border-b border-white py-3">
             <Text className="text-lg font-JakartaRegular">Pickup Time</Text>
             <Text className="text-lg font-JakartaRegular">
-              {formatTime(driverDetails?.time!)}
+              {formatTime(driverDetails?.time!  || 5! )}
             </Text>
           </View>
 
