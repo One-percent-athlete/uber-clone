7fc6c18c04febb8790efbb41d2632dd57082849b
One-percent-athlete <one.percent.athlete@gmail.com>
2025-03-13 23:32:09 +0900

    Update payment tsx

--- assets/components/Payment.tsx
+++ assets/components/Payment.tsx
@@ -2,9 +2,31 @@ import { View, Text } from "react-native";
 import CustomButton from "./CustomButton";
 
 const Payment = () => {
+    import { useStripe } from "@stripe/stripe-react-native";
+
+    export default function CheckoutScreen() {
+        const { initPaymentSheet, presentPaymentSheet } = useStripe();
+        const fetchPaymentSheetParams = async () => {
+          const response = await fetch("https://api.stripe.com/v1/payment_intents", {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+              Authorization: `Bearer ${stripePublishableKey}`,
+            },
+            body: JSON.stringify({
+              payment_method_types: ["card"],
+              amount: 1099,
+              currency: "usd",
+            }),
+          });
+          const { clientSecret } = await response.json();
+          return clientSecret;
+        }
   const openPaymenSheet = async () => {
     console.log("Payment Sheet Opened");
-  };
+    const clientSecret = await fetchPaymentSheetParams();
+    }
+
   return (
     <>
       <CustomButton
