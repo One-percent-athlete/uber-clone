06c5ad245c6fc8b202314d6b29aa4d5c9046f09d
One-percent-athlete <one.percent.athlete@gmail.com>
2025-03-13 23:41:44 +0900

    Update payment tsx

--- assets/components/Payment.tsx
+++ assets/components/Payment.tsx
@@ -4,47 +4,44 @@ import { useStripe } from "@stripe/stripe-react-native";
 import { stripePublishableKey } from "@/config";
 import { useEffect, useState } from "react";
 import { useLocationStore } from "@/store";
-
-
+import { fetchKey } from "@/lib/api";
 
 const Payment = () => {
+  const { initPaymentSheet, presentPaymentSheet } = useStripe();
+  const { userAddress, destinationAddress } = useLocationStore();
+  const [success, setSuccess] = useState(false);
+  const [error, setError] = useState(false);
 
-  export default function CheckoutScreen() {
-    const { initPaymentSheet, presentPaymentSheet } = useStripe();
-    const fetchPaymentSheetParams = async () => {
-      const response = await fetch(
-        "https://api.stripe.com/v1/payment_intents",
-        {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-            Authorization: `Bearer ${stripePublishableKey}`,
-          },
-          body: JSON.stringify({
-            payment_method_types: ["card"],
-            amount: 1099,
-            currency: "usd",
-          }),
-        },
-      );
-      const { clientSecret } = await response.json();
-      return clientSecret;
-    };
-    const openPaymenSheet = async () => {
-      console.log("Payment Sheet Opened");
-      const clientSecret = await fetchPaymentSheetParams();
-    };
+  const fetchPaymentSheetParams = async () => {
+    const response = await fetch("https://api.stripe.com/v1/payment_intents", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${stripePublishableKey}`,
+      },
+      body: JSON.stringify({
+        payment_method_types: ["card"],
+        amount: 1099,
+        currency: "usd",
+      }),
+    });
+    const { clientSecret } = await response.json();
+    return clientSecret;
+  };
+  const openPaymenSheet = async () => {
+    console.log("Payment Sheet Opened");
+    const clientSecret = await fetchPaymentSheetParams();
+  };
 
-    return (
-      <>
-        <CustomButton
-          title="Confirm Ride"
-          className="my-10"
-          onPress={openPaymenSheet}
-        />
-      </>
-    );
-  }
+  return (
+    <>
+      <CustomButton
+        title="Confirm Ride"
+        className="my-10"
+        onPress={openPaymenSheet}
+      />
+    </>
+  );
 };
 
 export default Payment;
