f9a4a3e51bc6d64ebc18944e369bcf5f149bf778
One-percent-athlete <one.percent.athlete@gmail.com>
2025-03-13 23:39:29 +0900

    Update payment tsx

--- assets/components/Payment.tsx
+++ assets/components/Payment.tsx
@@ -2,6 +2,8 @@ import { View, Text } from "react-native";
 import CustomButton from "./CustomButton";
 import { useStripe } from "@stripe/stripe-react-native";
 import { stripePublishableKey } from "@/config";
+import { useEffect, useState } from "react";
+import { useLocationStore } from "@/store";
 
 
 
