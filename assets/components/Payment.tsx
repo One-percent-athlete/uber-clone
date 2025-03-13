a82fac6cbd0ee5607228c3abeb839931439c5863
One-percent-athlete <one.percent.athlete@gmail.com>
2025-03-13 23:43:35 +0900

    Update payment tsx

--- assets/components/Payment.tsx
+++ assets/components/Payment.tsx
@@ -7,28 +7,8 @@ const Payment = () => {
   const { initPaymentSheet, presentPaymentSheet } = useStripe();
   const { userAddress, destinationAddress } = useLocationStore();
   const [success, setSuccess] = useState(false);
-  const [error, setError] = useState(false);
 
-  const fetchPaymentSheetParams = async () => {
-    const response = await fetch("https://api.stripe.com/v1/payment_intents", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${stripePublishableKey}`,
-      },
-      body: JSON.stringify({
-        payment_method_types: ["card"],
-        amount: 1099,
-        currency: "usd",
-      }),
-    });
-    const { clientSecret } = await response.json();
-    return clientSecret;
-  };
-  const openPaymenSheet = async () => {
-    console.log("Payment Sheet Opened");
-    const clientSecret = await fetchPaymentSheetParams();
-  };
+  
 
   return (
     <>
