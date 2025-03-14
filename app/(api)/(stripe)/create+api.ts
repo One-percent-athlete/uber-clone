c44e65698f658d5061498f42ba5f5a2470b0ef8d
One-percent-athlete <one.percent.athlete@gmail.com>
2025-03-14 20:28:12 +0900

    Update create api ts

--- app/(api)/(stripe)/create+api.ts
+++ app/(api)/(stripe)/create+api.ts
@@ -4,9 +4,9 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
 
 export async function POST(request: Request {
     const body = await request.json();
-    const args = { name, email, amount } = body;
+    const { name, email, amount } = body;
     if (!name || !email || !amount) {
-        return new Response(JSON.stringify({error: "Please enter a valid email address"}));
+        return new Response(JSON.stringify({error: "Please enter a valid email address", status: 400}));
     }
 })
 
