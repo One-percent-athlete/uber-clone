66a7f18590864ba80ad03d106a7141b0338e3fb2
One-percent-athlete <one.percent.athlete@gmail.com>
2025-03-14 20:18:23 +0900

    Update create api ts

--- app/(api)/(stripe)/create+api.ts
+++ app/(api)/(stripe)/create+api.ts
@@ -2,3 +2,11 @@ import { Stripe } from 'stripe';
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
 
+app.post('/api/stripe/create', async (req, res) => {
+  const { amount, currency } = req.body;
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount,
+    currency,
+  });
+  res.json({ client_secret: paymentIntent.client_secret });
+});
