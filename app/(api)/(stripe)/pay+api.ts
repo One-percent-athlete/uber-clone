8d4bec1441edca6e62a7f5f6ce6026d99d759593
One-percent-athlete <one.percent.athlete@gmail.com>
2025-03-15 23:58:30 +0900

    Create payment api ts

---
+++ app/(api)/(stripe)/pay+api.ts
@@ -0,0 +1,15 @@
+import { Stripe } from "stripe";
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
+
+export async function POST(request: Request) {
+  const body = await request.json();
+  const { name, email, amount } = body;
+  if (!name || !email || !amount) {
+    return new Response(
+      JSON.stringify({
+        error: "Please enter a valid email address",
+        status: 400,
+      }),
+    );
+  }
