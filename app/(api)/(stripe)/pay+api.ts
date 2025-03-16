111bb4016fb25e87f554627d985d554d38256009
One-percent-athlete <one.percent.athlete@gmail.com>
2025-03-16 18:56:25 +0900

    Update pay api ts

--- app/(api)/(stripe)/pay+api.ts
+++ app/(api)/(stripe)/pay+api.ts
@@ -5,7 +5,7 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
 export async function POST(request: Request) {
   const body = await request.json();
   const { payment_methods_id, payment_itent_id, customer_id } = body;
-  if (!name || !email || !amount) {
+  if (!payment_methods_id || !payment_itent_id || !customer_id) {
     return new Response(
       JSON.stringify({
         error: "Please enter a valid email address",
