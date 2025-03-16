5bb92213594fd9d6774c479a65f9a6ec85d396b7
One-percent-athlete <one.percent.athlete@gmail.com>
2025-03-16 19:00:47 +0900

    Update pay api ts

--- app/(api)/(stripe)/pay+api.ts
+++ app/(api)/(stripe)/pay+api.ts
@@ -4,8 +4,8 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
 
 export async function POST(request: Request) {
   const body = await request.json();
-  const { payment_methods_id, payment_itent_id, customer_id } = body;
-  if (!payment_methods_id || !payment_itent_id || !customer_id) {
+  const { payment_method_id, payment_itent_id, customer_id } = body;
+  if (!payment_method_id || !payment_itent_id || !customer_id) {
     return new Response(
       JSON.stringify({
         error: "Missing required payment information",
@@ -14,5 +14,7 @@ export async function POST(request: Request) {
     );
   }
 
-  const paymentMethod = await stripe.paymentMethods.attach()
+  const paymentMethod = await stripe.paymentMethods.attach(payment_method_id, {
+    customer: customer_id
+  })
 }
