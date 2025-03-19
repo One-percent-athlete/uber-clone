074e02b1f94a4616539fd789ca265cdc6dba0e26
One-percent-athlete <one.percent.athlete@gmail.com>
2025-03-19 17:20:42 +0900

    Update auth ts

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,34 +1,40 @@
-import * as SecureStore from 'expo-secure-store'
-import { Platform } from 'react-native'
-import { TokenCache } from '@clerk/clerk-expo/dist/cache'
+import * as SecureStore from "expo-secure-store";
+import { Linking, Platform } from "react-native";
+import { TokenCache } from "@clerk/clerk-expo/dist/cache";
 
 export const tokenCache = (): TokenCache => {
   return {
     getToken: async (key: string) => {
       try {
-        const item = await SecureStore.getItemAsync(key)
+        const item = await SecureStore.getItemAsync(key);
         if (item) {
-          console.log(`${key} was used 🔐 \n`)
+          console.log(`${key} was used 🔐 \n`);
         } else {
-          console.log('No values stored under key: ' + key)
+          console.log("No values stored under key: " + key);
         }
-        return item
+        return item;
       } catch (error) {
-        console.error('secure store get item error: ', error)
-        await SecureStore.deleteItemAsync(key)
-        return null
+        console.error("secure store get item error: ", error);
+        await SecureStore.deleteItemAsync(key);
+        return null;
       }
     },
     saveToken: (key: string, token: string) => {
-      return SecureStore.setItemAsync(key, token)
+      return SecureStore.setItemAsync(key, token);
     },
-  }
-}
+  };
+};
 
 export const googleOAuth = async (startOAuthFlow: any) => {
   try {
-    
+    const { createdSessionId, signOut, setActive } = await startOAuthFlow({
+      redirectUrl: Linking.createURL("/dashboard", { scheme: "myapp" }),
+    });
+    if (createdSessionId) {
+      setActive!({ session: createdSessionId });
+    } else {
+    }
   } catch (error) {
-    
+    console.log(error);
   }
-}
+};
