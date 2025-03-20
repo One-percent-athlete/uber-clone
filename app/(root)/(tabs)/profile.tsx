a71139ad8506c10942ad3562636bad233ce6157c
One-percent-athlete <one.percent.athlete@gmail.com>
2025-03-20 20:07:29 +0900

    Update profile tsx

--- app/(root)/(tabs)/profile.tsx
+++ app/(root)/(tabs)/profile.tsx
@@ -1,8 +1,7 @@
+import InputField from "@/assets/components/InputField";
 import { useUser } from "@clerk/clerk-expo";
 import { Image, ScrollView, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
-
-import InputField from "@/components/input-field";
 
 const Profile = () => {
   const { user } = useUser();
