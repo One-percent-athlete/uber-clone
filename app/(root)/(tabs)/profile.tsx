0d02dea0b15543a9f07ff6b8f4e6ab337ae4ad78
One-percent-athlete <one.percent.athlete@gmail.com>
2025-03-20 20:02:54 +0900

    Update profile tsx

--- app/(root)/(tabs)/profile.tsx
+++ app/(root)/(tabs)/profile.tsx
@@ -1,12 +1,17 @@
-import { View, Text } from "react-native"
-import { SafeAreaView } from "react-native-safe-area-context";
+import {useUser} from "@clerk/clerk-expo";
+import {Image, ScrollView, Text, View} from "react-native";
+import {SafeAreaView} from "react-native-safe-area-context";
+
+import InputField from "@/components/input-field";
 
 const Profile = () => {
-  return (
-    <SafeAreaView>
-      <Text>Profile</Text>
-    </SafeAreaView>
-  );
-}
+    const {user} = useUser();
 
-export default Profle
+    return (
+        <SafeAreaView className="flex-1">
+            
+        </SafeAreaView>
+    );
+};
+
+export default Profile;
