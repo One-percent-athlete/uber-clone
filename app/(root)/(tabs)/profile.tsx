8379c7dbcf3c9e133db3d7b68f4289eb033c61c8
One-percent-athlete <one.percent.athlete@gmail.com>
2025-03-20 20:03:48 +0900

    Update profile tsx

--- app/(root)/(tabs)/profile.tsx
+++ app/(root)/(tabs)/profile.tsx
@@ -7,7 +7,18 @@ import InputField from "@/components/input-field";
 const Profile = () => {
   const { user } = useUser();
 
-  return <SafeAreaView className="flex-1"></SafeAreaView>;
+  return (
+    <SafeAreaView className="flex-1">
+      <ScrollView
+        className="px-5"
+        contentContainerStyle={{ paddingBottom: 120 }}
+      >
+        <Text className="text-2xl font-JakartaBold my-5">My profile</Text>
+
+        
+      </ScrollView>
+    </SafeAreaView>
+  );
 };
 
 export default Profile;
