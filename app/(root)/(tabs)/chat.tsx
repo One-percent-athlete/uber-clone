ca4138d43319c4bd84fbc9e939557e5752d29602
One-percent-athlete <one.percent.athlete@gmail.com>
2025-03-23 14:23:43 +0900

    Update chat tsx

--- app/(root)/(tabs)/chat.tsx
+++ app/(root)/(tabs)/chat.tsx
@@ -3,6 +3,12 @@ import {SafeAreaView} from "react-native-safe-area-context";
 
 import {images} from "@/constants";
 
-
+const Chat = () => {
+    return (
+        <SafeAreaView className="flex-1 bg-white p-5">
+            
+        </SafeAreaView>
+    );
+};
 
 export default Chat;
