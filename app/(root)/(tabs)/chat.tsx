4ff4d6bb6c1ac73809218656e4ca9dd6a4648cd5
One-percent-athlete <one.percent.athlete@gmail.com>
2025-03-23 14:27:08 +0900

    Update chat tsx

--- app/(root)/(tabs)/chat.tsx
+++ app/(root)/(tabs)/chat.tsx
@@ -1,25 +1,24 @@
-import {Image, ScrollView, Text, View} from "react-native";
-import {SafeAreaView} from "react-native-safe-area-context";
+import { Image, ScrollView, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
-import {images} from "@/constants";
+import { images } from "@/constants";
 
 const Chat = () => {
-    return (
-        <SafeAreaView className="flex-1 bg-white p-5">
-            <ScrollView contentContainerStyle={{flexGrow: 1}}>
-                <Text className="text-2xl font-JakartaBold">Chat</Text>
-                <View className="flex-1 h-fit flex justify-center items-center">
-                    <Image
-                        source={images.message}
-                        alt="message"
-                        className="w-full h-40"
-                        resizeMode="contain"
-                    />
-                    
-                </View>
-            </ScrollView>
-        </SafeAreaView>
-    );
+  return (
+    <SafeAreaView className="flex-1 bg-white p-5">
+      <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
+        <Text className="text-2xl font-JakartaBold">Chat</Text>
+        <View className="flex-1 h-fit flex justify-center items-center">
+          <Image
+            source={images.message}
+            alt="message"
+            className="w-full h-40"
+            resizeMode="contain"
+          />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
 };
 
 export default Chat;
