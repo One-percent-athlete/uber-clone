9460dbbf165da61859cf4e7a00d70b3130e72605
One-percent-athlete <one.percent.athlete@gmail.com>
2025-03-23 14:22:59 +0900

    Update chat tsx

---
+++ app/(root)/(tabs)/chat.tsx
@@ -0,0 +1,8 @@
+import {Image, ScrollView, Text, View} from "react-native";
+import {SafeAreaView} from "react-native-safe-area-context";
+
+import {images} from "@/constants";
+
+
+
+export default Chat;
