7ca750911b040fd4a831c7fe3e352f8f77f79135
One-percent-athlete <one.percent.athlete@gmail.com>
2025-03-24 21:15:25 +0900

    Update ride card ts

--- assets/components/RideCard.tsx
+++ assets/components/RideCard.tsx
@@ -21,18 +21,24 @@ const RideCard = ({
     <View className="flex flex-col items-center justify-center p-3">
       <View className="flex flex-row items-center justify-between">
         <Image
-            source={{ uri: `https://maps.geoapify.com/v1/staticmap?style=osm-bright&width=600&height=400&center=lonlat:${destination_longitude},${destination_latitude}&zoom=14&apiKey=${process.env.EXPO_PUBLIC_GEOAPIFY_API_KEY}` }}
-            className="w-[80px] h-[90px] rounded-lg"
+          source={{
+            uri: `https://maps.geoapify.com/v1/staticmap?style=osm-bright&width=600&height=400&center=lonlat:${destination_longitude},${destination_latitude}&zoom=14&apiKey=${process.env.EXPO_PUBLIC_GEOAPIFY_API_KEY}`,
+          }}
+          className="w-[80px] h-[90px] rounded-lg"
         />
         <View className="flex flex-col mx-5 gap-y-5 flex-1">
           <View className="flex flex-row items-center gap-x-2">
             <Image source={icons.to} className="w-5 h-5" />
-            <Text className="text-md font-JakartaMedium" numberOfLines={1}>{origin_address}</Text>
+            <Text className="text-md font-JakartaMedium" numberOfLines={1}>
+              {origin_address}
+            </Text>
           </View>
-            
+
           <View className="flex flex-row items-center gap-x-2">
             <Image source={icons.point} className="w-5 h-5" />
-            <Text className="text-md font-JakartaMedium" numberOfLines={1}>{destination_address}</Text>
+            <Text className="text-md font-JakartaMedium" numberOfLines={1}>
+              {destination_address}
+            </Text>
           </View>
         </View>
       </View>
@@ -74,6 +80,6 @@ const RideCard = ({
       </View>
     </View>
   </View>
-)
+);
 
 export default RideCard;
